9614615504e6bed111469ed0801e1d4e9051ed38
EkoAlex <alexandru@appticles.com>
2017-07-10 14:59:00 +0300

    Remove script that is added automatically

--- frontend/themes/obliq/single.php
+++ frontend/themes/obliq/single.php
@@ -6,7 +6,6 @@
 
 		<?php do_action( 'amp_post_template_head', $this ); ?>
 
-		<script custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js" async=""></script>
 		<script custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js" async=""></script>
 		<script custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js" async=""></script>
 		<script custom-element="amp-instagram" src="https://cdn.ampproject.org/v0/amp-instagram-0.1.js" async=""></script>
