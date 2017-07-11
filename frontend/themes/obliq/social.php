76d20f3abf35178b7ab2b5c90bea4ce5826b71b2
EkoAlex <alexandru@appticles.com>
2017-07-11 13:46:42 +0300

    Fix typo

--- frontend/themes/obliq/social.php
+++ frontend/themes/obliq/social.php
@@ -8,7 +8,7 @@ $facebook_app_id = $wp_amp_themes_options->get_setting( 'facebook_app_id' );
 <!-- start Socialbox -->
 <div class="ampstart-social-box py3">
 <amp-social-share type="twitter"></amp-social-share>
-<amp-social-share type="facebook" data-param-app-id="<?php echo $facebook_app_id; ?>"></amp-social-share>
+<amp-social-share type="facebook" data-param-app_id="<?php echo $facebook_app_id; ?>"></amp-social-share>
 <amp-social-share type="gplus"></amp-social-share>
 
 <amp-social-share type="pinterest"></amp-social-share>
